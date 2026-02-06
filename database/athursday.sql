359a4c4848b377bd5985efbd0d91ca651b9396d6
root <root@srv1294100.hstgr.cloud>
2026-02-06 06:14:55 +0000

    Update home routes and database changes

--- database/athursday.sql
+++ database/athursday.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 24, 2026 at 06:12 AM
+-- Generation Time: Feb 05, 2026 at 10:43 AM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -49,6 +49,33 @@ INSERT INTO `about_us` (`id`, `title`, `description`, `image`, `status`, `create
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `banners`
+--
+
+DROP TABLE IF EXISTS `banners`;
+CREATE TABLE IF NOT EXISTS `banners` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `title`, `image_url`, `start_time`, `end_time`, `is_active`, `created_at`) VALUES
+(16, 'aaaa', '/assets/img/1769776361431-386274838625800.jpg.jpeg', '2026-01-30 18:02:00', '2026-02-14 18:02:00', 1, '2026-01-30 12:32:41'),
+(17, 'bbbb', '/assets/img/1769776392572-386274798625797.jpg.jpeg', '2026-01-30 18:02:00', '2026-02-14 18:02:00', 1, '2026-01-30 12:33:12'),
+(18, 'cccc', '/assets/img/1769776468180-val-8-design-template-3460db2d6ff6bd4275c844d2e98393fascreen.jpg', '2026-01-30 18:04:00', '2026-02-14 18:04:00', 1, '2026-01-30 12:34:28');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `beans_wallet`
 --
 
@@ -63,6 +90,37 @@ CREATE TABLE IF NOT EXISTS `beans_wallet` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blogs`
+--
+
+DROP TABLE IF EXISTS `blogs`;
+CREATE TABLE IF NOT EXISTS `blogs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `is_approved` tinyint(1) DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `blogs`
+--
+
+INSERT INTO `blogs` (`id`, `user_id`, `title`, `image`, `content`, `is_approved`, `created_at`) VALUES
+(1, 3, 'hiuhihih', '1769777613697-blog.jpg', 'hjojh hohod sdohc 8vhsz jvoz icvohzoc jz uhziu', 1, '2026-01-30 12:53:34'),
+(2, 3, 'iuhodsaonc', '1770016773463-blog.jpg', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\",', 1, '2026-02-02 07:19:33'),
+(3, 3, 'fdsdfsdc', '1770020113242-blog.png', '1914 translation by H. Rackham\r\n\"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?\"', 1, '2026-02-02 08:15:13'),
+(4, 3, 'hjfgh', '1770021622762-blog.png', 'Section 1.10.33 of \"de Finibus Bonorum et Malorum\", written by Cicero in 45 BC\r\n\"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.\"', 1, '2026-02-02 08:40:22'),
+(5, 3, 'xzzdsfszd', '1770021664488-blog.jpg', 'dfgdfgxfghdfg yuyt yutty sdgsf sd', 1, '2026-02-02 08:41:04'),
+(6, 3, 'dsgfsfgngfd', '1770030662696-blog.jpg', 'nijobi uf78y98 yf87h 7y89jo', 0, '2026-02-02 11:11:02'),
+(7, 3, 'iihiubihb', '1770030952318-blog.jpg', 'jyihibi ohuoijo g9o fhi es5rf7u es65f7 oh oinkvu uyhkjbhg', 0, '2026-02-02 11:15:52');
 
 -- --------------------------------------------------------
 
@@ -102,27 +160,25 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `earn_beans` int NOT NULL,
-  `redeem_beans` int NOT NULL,
+  `main_category_id` int NOT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_main_category` (`main_category_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `categories`
 --
 
-INSERT INTO `categories` (`id`, `name`, `earn_beans`, `redeem_beans`, `created_at`) VALUES
-(1, 'Coffee', 0, 0, '2026-01-20 06:50:38'),
-(2, ' Cold Brew & Iced Coffee', 0, 0, '2026-01-20 06:57:03'),
-(3, 'Hot Chocolate', 0, 0, '2026-01-20 06:57:20'),
-(4, 'Food – Starters', 0, 0, '2026-01-20 06:57:35'),
-(5, 'Food – Wraps & Sandwiches', 0, 0, '2026-01-20 06:57:48'),
-(6, 'Food – Platters', 0, 0, '2026-01-20 06:57:59'),
-(7, 'Food – Pasta', 0, 0, '2026-01-20 06:58:12'),
-(8, 'Food – Tacos', 0, 0, '2026-01-20 06:58:21'),
-(9, ' Food – French Fries', 0, 0, '2026-01-20 06:58:30'),
-(10, 'Food – Continental Snacks', 0, 0, '2026-01-20 06:58:39');
+INSERT INTO `categories` (`id`, `name`, `main_category_id`, `created_at`) VALUES
+(1, 'Cold Coffee', 1, '2026-01-29 05:33:12'),
+(2, 'food', 2, '2026-01-29 05:41:39'),
+(3, 'coffe', 3, '2026-01-29 05:45:28'),
+(4, 'mocka', 2, '2026-01-29 06:02:22'),
+(7, 'dgsrdgf', 3, '2026-01-29 06:26:50'),
+(8, 'dsfsadf', 2, '2026-01-29 06:35:13'),
+(9, 'dsfd', 3, '2026-01-29 06:41:02'),
+(10, 'gfhdfg', 4, '2026-01-29 12:07:32');
 
 -- --------------------------------------------------------
 
@@ -168,6 +224,47 @@ CREATE TABLE IF NOT EXISTS `feedbacks` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ;
+
+--
+-- Dumping data for table `feedbacks`
+--
+
+INSERT INTO `feedbacks` (`id`, `user_id`, `rating`, `message`, `created_at`) VALUES
+(1, 3, 4, 'Best caffe', '2026-01-24 08:18:03'),
+(2, 3, 4, 'Best caffe', '2026-01-24 08:18:15'),
+(3, 3, 4, 'A Thursday Café truly feels like a mid-week escape. The coffee is rich, perfectly brewed, and consistently satisfying. The ambience is calm and aesthetic, making it ideal for both work and relaxed conversations. The staff is warm and attentive, which adds a personal touch to the experience. Every visit feels refreshing and worth repeating.', '2026-01-24 08:29:14'),
+(4, 3, 3, 'This café has quickly become one of my favorite hangout spots. The peaceful ambience, reliable Wi-Fi, and delicious coffee make it ideal for meetings or casual visits. The staff is friendly and professional, and the overall vibe is very relaxing. A Thursday Café truly stands out for its consistency and charm.', '2026-01-24 08:29:39'),
+(5, 3, 4, 'Lowkey obsessed with A Thursday Café. The coffee is always on point and the vibe is super chill. Perfect place to sit, relax, or just zone out with a good cup of coffee. The ambience feels cozy without trying too hard. Definitely my go-to spot now.', '2026-01-24 08:34:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `main_categories`
+--
+
+DROP TABLE IF EXISTS `main_categories`;
+CREATE TABLE IF NOT EXISTS `main_categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `slug` varchar(50) NOT NULL,
+  `earn_beans` int DEFAULT '0',
+  `redeem_beans` int DEFAULT '0',
+  `status` tinyint DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `main_categories`
+--
+
+INSERT INTO `main_categories` (`id`, `name`, `slug`, `earn_beans`, `redeem_beans`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Regular', 'regular', 25, 30, 0, '2026-01-27 05:59:23', '2026-01-28 13:34:21'),
+(2, 'Premium ', 'premium ', 15, 10, 1, '2026-01-28 13:29:57', '2026-01-28 13:34:32'),
+(3, 'Ultra Premium ', 'ultra premium ', 8, 10, 1, '2026-01-28 13:34:12', '2026-01-28 13:34:12'),
+(4, 'Regular ', 'regular ', 20, 50, 1, '2026-01-29 06:10:49', '2026-01-29 06:11:11');
 
 -- --------------------------------------------------------
 
@@ -322,7 +419,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `status` tinyint DEFAULT '1',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `products`
@@ -381,7 +478,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `mobile` (`mobile`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
@@ -392,7 +489,9 @@ INSERT INTO `users` (`id`, `username`, `email`, `mobile`, `password`, `otp`, `cr
 (2, 'sam1', 'sam1@phian.com', '1111111111', '$2b$10$CvHxNBm3SK/ovbFTd50F/em3t2BWrdXPDEs12chy5b46OT89LMYgC', NULL, '2026-01-13 12:21:05'),
 (3, 'tom', 'tom@phian.com', '1236541111', '$2b$10$fZLq5vCQhKUxVRPru2PStOCkwhOUhKphHjfYoGgrtuPraM2bpIqNC', NULL, '2026-01-13 12:22:35'),
 (4, 'Kumar', 'kumar@uhu.cic', '9876543210', '$2b$10$pvUYO/jdcSr9zBhS1Wr9c.odZG3IX5Wkw.5FUbyM1/TiwOCTzOGRW', NULL, '2026-01-21 10:35:16'),
-(5, 'Kumar', 'kumar1@uhu.cic', '9876543211', '$2b$10$ajJHLr3O/UUfYnCYR6fdS.cwNnqbRtKC7s1jn3gkwdCybp66tmPom', NULL, '2026-01-21 10:55:45');
+(5, 'Kumar', 'kumar1@uhu.cic', '9876543211', '$2b$10$ajJHLr3O/UUfYnCYR6fdS.cwNnqbRtKC7s1jn3gkwdCybp66tmPom', NULL, '2026-01-21 10:55:45'),
+(6, 'Phian', 'admin@phian.com', '1234567890', '$2b$10$8fD5h3x4Rnd20MOgHqSmUubduARbF5P6yH8x9inEfSBkTdt5p4242', NULL, '2026-02-02 11:42:37'),
+(7, 'Phian', 'admin1@phian.com', '1234567899', '$2b$10$1UBjF5IphX48YZnV0ZMnYekesaPm5hps9wx30pmukicogmoLr4TgK', NULL, '2026-02-02 11:56:50');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
